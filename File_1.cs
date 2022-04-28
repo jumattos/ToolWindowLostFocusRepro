de5d4ae92b07b1ceaee8980614595580dac2caf6
Jullyana Mattos Vycas <jumattos@microsoft.com>
2022-04-28 11:48:16 -0700

    Setup files for repro

---
+++ File_1.cs
@@ -0,0 +1,10 @@
+﻿namespace ToolWindowLostFocusRepro
+{
+    /// <summary>
+    /// Although File_1 is the left most tab, focus will be restored to File_2,
+    /// because it was the last focused editor file.
+    /// </summary>
+    internal class File_1
+    {
+    }
+}
---
+++ File_2.cs
@@ -0,0 +1,9 @@
+﻿namespace ToolWindowLostFocusRepro
+{
+    /// <summary>
+    /// Although this window was not the one active during the reload, it "steals" focus.
+    /// </summary>
+    internal class File_2
+    {
+    }
+}
